90ca7b46962871a31a5a1a2dc97555f67b9b097c
Anastasia Lesnikova <vassilisa789@gmail.com>
2024-12-10 12:43:48 +0300

    4th commit: realized class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+import entity.Person;
+import serveses.CSVToList;
+import java.util.List;
+
+
 public class Main {
+
+    public static final String PATH = "C://Users//Анастасия//IdeaProjects//Collection2//src//main//resources//foreign_names.csv";
+
+    public static void main(String[] args){
+        CSVToList parser = CSVToList.CSV_PARSER;
+        List<Person> personList = parser.toPersonList(PATH, ';');
+        System.out.println(personList.size());
+    }
 }
