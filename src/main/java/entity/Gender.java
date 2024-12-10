dd5d7b394de1ec87c8149fd6552fb46c8179caed
Anastasia Lesnikova <vassilisa789@gmail.com>
2024-12-10 12:31:02 +0300

    3d commit: realized class CSVToList and enum Gender

--- src/main/java/entity/Gender.java
+++ src/main/java/entity/Gender.java
@@ -1,4 +1,6 @@
 package entity;
 
 public enum Gender {
+    MALE,
+    FEMALE;
 }
--- src/main/java/serveses/CSVToList.java
+++ src/main/java/serveses/CSVToList.java
@@ -1,4 +1,50 @@
 package serveses;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import entity.Division;
+import entity.Gender;
+import entity.Person;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVToList {
+
+    public static final CSVToList CSV_PARSER = new CSVToList();
+
+    private CSVToList() {
+    }
+
+    public List<Person> toPersonList(String CSVFilePath, char separator) {
+        List<Person> personList = new ArrayList<>();
+        CSVParser parser = new CSVParserBuilder().withSeparator(separator).build();
+
+        try (FileReader fileReader = new FileReader(CSVFilePath);
+             CSVReader csvReader = new CSVReaderBuilder(fileReader).withCSVParser(parser).build()) {
+            String[] next;
+            while ((next = csvReader.readNext()) != null){
+                try {
+                    int id = Integer.parseInt(next[0]);
+                    int salary = Integer.parseInt(next[5]);
+                    Gender gender = Gender.valueOf(next[2].toUpperCase());
+                    Division division = Division.divisionFabric(next[4]);
+                    Person person = new Person(id, next[1], gender, next[3], division, salary);
+                    personList.add(person);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+        return personList;
+    }
 }
